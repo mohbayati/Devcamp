8b51935b084884f1a7bf4262b02ce694efdb93dc
Mohammad Bayati <Mohammad.Bayati@SLATERGORDON.UK>
2023-01-16 16:35:28 +0000

    Added error Handling and Mangoose middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,75 +1,63 @@
+const asyncHandler = require("../middleware/async");
 const Bootcamp = require("../models/Bootcamp");
+const ErrorResponse = require("../utils/errorResponse");
 
 //@desc         Get all bootcamps
 //@route        Get /api/v1/bootcamps
 //@access       Public
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    res
-      .status(200)
-      .json({ sucess: true, count: bootcamps.length, data: bootcamps });
-  } catch (error) {
-    res.status(400).json({ sucess: false, err: error });
-  }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res
+    .status(200)
+    .json({ sucess: true, count: bootcamps.length, data: bootcamps });
+});
 
 //@desc         Get single bootcamp
 //@route        Get /api/v1/bootcamps/:id
 //@access       Public
-exports.getBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findById(req.params.id);
-    if (!bootcamp) {
-      res.status(404).json({ sucess: false, err: "not found" });
-    }
-    res.status(200).json({ sucess: true, data: bootcamp });
-  } catch (error) {
-    res.status(400).json({ sucess: false, err: error });
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ sucess: true, data: bootcamp });
+});
 
 //@desc         Create new bootcamp
 //@route        POST /api/v1/bootcamps
 //@access       Private
-exports.createBootcamps = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({ sucess: true, data: bootcamp });
-  } catch (error) {
-    res.status(400).json({ sucess: false, err: error });
-  }
-};
+exports.createBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({ sucess: true, data: bootcamp });
+});
 
 //@desc         Update bootcamp
 //@route        PUT /api/v1/bootcamps
 //@access       Private
-exports.updateBootcamps = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!bootcamp) {
-      res.status(404).json({ sucess: false, err: "not found" });
-    }
-    res.status(200).json({ sucess: true, data: bootcamp });
-  } catch (error) {
-    res.status(400).json({ sucess: false, err: error });
+exports.updateBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ sucess: true, data: bootcamp });
+});
 
 //@desc         Delete bootcamp
 //@route        DELETE /api/v1/bootcamps
 //@access       Private
-exports.deleteBootcamps = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-    if (!bootcamp) {
-      res.status(404).json({ sucess: false, err: "not found" });
-    }
-    res.status(200).json({ sucess: true, data: {} });
-  } catch (error) {
-    res.status(400).json({ sucess: false, err: error });
+exports.deleteBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ sucess: true, data: {} });
+});
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -98,5 +99,10 @@ const BootcampSchema = new mongoose.Schema({
   },
 });
 
-module.exports =
-  mongoose.models.Bootcamp || mongoose.model("Bootcamp", BootcampSchema);
+//Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+module.exports = mongoose.model("Bootcamp", BootcampSchema);
