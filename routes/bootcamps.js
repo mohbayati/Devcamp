d5c28fb34d23eed04973aa84fb37e35b8d658557
Mohammad Bayati <Mohammad.Bayati@SLATERGORDON.UK>
2023-01-18 12:24:27 +0000

    Added CRUD for couese

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,18 +2,28 @@ const express = require("express");
 const {
   getBootcamps,
   createBootcamps,
-  deleteBootcamps,
+  getBootcampsInRadius,
   getBootcamp,
   updateBootcamps,
+  deleteBootcamp,
 } = require("../controllers/bootcamps");
+
+// Include other resource routers
+const courseRouter = require("./courses");
 
 const router = express.Router();
 
+//Re-route into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
+
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+
 router.route("/").get(getBootcamps).post(createBootcamps);
+
 router
   .route("/:id")
   .put(updateBootcamps)
   .get(getBootcamp)
-  .delete(deleteBootcamps);
+  .delete(deleteBootcamp);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: "./config/config.env" });
 
 //Load  models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 //Connect to database
 mongoose.set("strictQuery", false);
@@ -16,11 +17,15 @@ mongoose.connect(process.env.MONGO_URI);
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 //Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data Import".green.inverse);
     process.exit();
   } catch (err) {
@@ -32,6 +37,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data Destroyed...".red.inverse);
     process.exit();
   } catch (err) {
