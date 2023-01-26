be426731bb9aa721b3affd6952192d9100abf2c8
Mohammad Bayati <Mohammad.Bayati@SLATERGORDON.UK>
2023-01-26 14:30:47 +0000

    Added users and authorization

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,6 +28,21 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@route        POST /api/v1/bootcamps
 //@access       Private
 exports.createBootcamps = asyncHandler(async (req, res, next) => {
+  // Add user to req, body
+  req.body.user = req.user.id;
+
+  //Check for publiush bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({ sucess: true, data: bootcamp });
 });
@@ -36,7 +51,7 @@ exports.createBootcamps = asyncHandler(async (req, res, next) => {
 //@route        PUT /api/v1/bootcamps
 //@access       Private
 exports.updateBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+  let bootcamp = await Bootcamp.findById(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
@@ -45,6 +60,20 @@ exports.updateBootcamps = asyncHandler(async (req, res, next) => {
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is bootcamo owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
   res.status(200).json({ sucess: true, data: bootcamp });
 });
 
@@ -56,6 +85,15 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  // Make sure user is bootcamo owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
     );
   }
   bootcamp.remove();
@@ -97,6 +135,16 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamo owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
     );
   }
 
--- controllers/courses.js
+++ controllers/courses.js
@@ -49,6 +49,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 
 exports.createCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
   if (!bootcamp) {
@@ -59,7 +60,15 @@ exports.createCourse = asyncHandler(async (req, res, next) => {
       )
     );
   }
-
+  // Make sure user is bootcamo owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add course to this bootcamp ${bootcamp._id}`,
+        401
+      )
+    );
+  }
   const course = await Course.create(req.body);
 
   res.status(200).json({
@@ -78,6 +87,16 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamo owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course  ${course._id}`,
+        401
+      )
     );
   }
 
